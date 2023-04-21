21f9781350e721f4e2e1c77ee9d63a0b2ae474e5
alok seth <alok.seth4@gmail.com>
2023-04-21 12:45:45 +0530

    first commit

---
+++ frontend/src/component/App.js
@@ -0,0 +1,14 @@
+import './App.css';
+
+function App() {
+  return (
+    <div>
+      <header>
+        <a href="/">Marks Distribution</a>
+      </header>
+      <main>List of Student</main>
+    </div>
+  );
+}
+
+export default App;
